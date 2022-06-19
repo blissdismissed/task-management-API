fae80290673f54c45aff727678889fda29bc5c05
ajautz@gmail.com <ajautz@gmail.com>
2022-06-19 11:21:32 -0400

    add todos controller actions for get and add todos

---
+++ models/todo.model.js
@@ -0,0 +1,40 @@
+const mongodb = require('mongodb');
+
+const db = require('../data/database');
+
+class Todo {
+  constructor(text, id) {
+    this.text = text;
+    this.id = id;
+  }
+
+  static async getAllTodos() {
+    const todoDocuments = await db.getDb().collection('todos').find().toArray();
+    return todoDocuments.map(todoDocument => new Todo( todoDocument._id, todoDocument._text ));
+  }
+
+  save() { 
+    if (this.id) {
+      const todoId = new mongodb.ObjectId(this.id);
+      return db.getDb().collection('todos').updateOne({ _id: todoId }, {
+        $set: {
+          text: this.text
+        }
+      });
+    } else {
+      return db.getDb().collection('todos').insertOne({ text: this.text });
+    }
+  }
+
+  delete() {
+    if (this.id) {
+      throw new Error("Trying to delete an id that doesn't exist!");
+    }
+
+    const todoId = new mongodb.ObjectId(this.id);
+    return db.getDd().collection('todos').deleteOne({ _id: todoId });
+  }
+}
+
+module.exports = Todo;
+
